de5978121427d584684329d9a7cec9708ace851c
Joshua King <windosbk+github@gmail.com>
2017-01-06 22:36:33 +1300

    help and pssharper

--- BurntToast/Public/Basic/New-BTAppId.ps1
+++ BurntToast/Public/Basic/New-BTAppId.ps1
@@ -2,10 +2,17 @@
 {
     <#
         .SYNOPSIS
+        Creates a new AppId Registry Key.
 
         .DESCRIPTION
-        
+        The New-BTAppId function create a new AppId registry key in the Current User's Registery Hive. If the desired AppId is already present in the Registry then no changes are made.
+
+        If no AppId is specified then the AppId specified in the config.json file in the BurntToast module's root directory is used.
+
         .PARAMETER AppId
+        Specifies the new AppId. You can use any alphanumeric characters.
+
+        Defaults to the AppId specified in the config.json file in the BurntToast module's root directoy if not provided.
 
         .INPUTS
         System.String
@@ -19,16 +26,22 @@
         The New-BTAppId function is classified as: Basic
 
         .EXAMPLE
+        New-BTAppId
+
+        This command creates an AppId registry key using the value specified in the BurntToast module's config.json file, which is 'BurntToast' by default.
 
         .EXAMPLE
+        New-BTAppId -AppId 'Script Checker'
+
+        This command create an AppId registry key called 'Script Checker.'
         
         .LINK
         https://github.com/Windos/BurntToast/blob/master/Help/New-BTAppId.md
     #>
 
+    [CmdletBinding()]
     param
     (
-        [Parameter()]
         [ValidateNotNullOrEmpty()] 
         [string] $AppId = $Script:Config.AppId
     )
