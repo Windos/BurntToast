6ef1b7580014434e126450c5a06d71b8c5879c85
Windos <windosbk+github@gmail.com>
2019-02-03 21:35:10 +1300

    Trimming the fat

--- Tests/BurntToast.Tests.ps1
+++ Tests/BurntToast.Tests.ps1
@@ -2,90 +2,63 @@ Import-Module "$PSScriptRoot/../BurntToast/BurntToast.psd1" -Force
 
 Describe 'New-BTAction' {
     Context 'running without arguments' {
-        It 'runs without errors' {
-            { New-BTAction } | Should Not Throw
-        }
-
         It 'returns a [ToastActionsCustom] object' {
             New-BTAction | Should BeOfType 'Microsoft.Toolkit.Uwp.Notifications.ToastActionsCustom'
         }
 
-        if ($env:TF_BUILD) {
-            Start-Transcript tmp.log
-            try {
-                New-BTAction -WhatIf
-            }
-            finally {
-                Stop-Transcript
-                $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
-                Remove-Item tmp.log
-            }
-
-            It 'has consitent WhatIf response' {
-                $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 0 Buttons, and 0 ContextMenuItems".'
-                $Log -join [System.Environment]::NewLine | Should Be $Expected
-            }
+        Start-Transcript tmp.log
+        try {
+            New-BTAction -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 0 Buttons, and 0 ContextMenuItems".'
+            $Log -join [System.Environment]::NewLine | Should Be $Expected
         }
     }
     Context 'snooze and dismiss modal' {
-        It 'runs without errors' {
-            { New-BTAction -SnoozeAndDismiss } | Should Not Throw
-        }
-
         It 'returns a [ToastActionsSnoozeAndDismiss] object' {
             New-BTAction -SnoozeAndDismiss | Should BeOfType 'Microsoft.Toolkit.Uwp.Notifications.ToastActionsSnoozeAndDismiss'
         }
 
-        if ($env:TF_BUILD) {
-            Start-Transcript tmp.log
-            try {
-                New-BTAction -SnoozeAndDismiss -WhatIf
-            }
-            finally {
-                Stop-Transcript
-                $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
-                Remove-Item tmp.log
-            }
-
-            It 'has consitent WhatIf response' {
-                $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsSnoozeAndDismiss] with 0 Inputs, 0 Buttons, and 0 ContextMenuItems".'
-                $Log -join [System.Environment]::NewLine | Should Be $Expected
-            }
+        Start-Transcript tmp.log
+        try {
+            New-BTAction -SnoozeAndDismiss -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsSnoozeAndDismiss] with 0 Inputs, 0 Buttons, and 0 ContextMenuItems".'
+            $Log -join [System.Environment]::NewLine | Should Be $Expected
         }
     }
     Context 'single clickable button' {
-        It 'runs without errors' {
-            { New-BTAction -Buttons (New-BTButton -Content 'Google' -Arguments 'https://google.com') } | Should Not Throw
-        }
-
         It 'returns a [ToastActionsCustom] object' {
             New-BTAction -Buttons (New-BTButton -Content 'Google' -Arguments 'https://google.com') | Should BeOfType 'Microsoft.Toolkit.Uwp.Notifications.ToastActionsCustom'
         }
 
-        if ($env:TF_BUILD) {
-            Start-Transcript tmp.log
-            try {
-                New-BTAction -Buttons (New-BTButton -Content 'Google' -Arguments 'https://google.com') -WhatIf
-            }
-            finally {
-                Stop-Transcript
-                $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
-                Remove-Item tmp.log
-            }
-
-            It 'has consitent WhatIf response' {
-                $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 1 Buttons, and 0 ContextMenuItems".'
-                $Log -join [System.Environment]::NewLine | Should Be $Expected
-            }
+        Start-Transcript tmp.log
+        try {
+            New-BTAction -Buttons (New-BTButton -Content 'Google' -Arguments 'https://google.com') -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 1 Buttons, and 0 ContextMenuItems".'
+            $Log -join [System.Environment]::NewLine | Should Be $Expected
         }
     }
     Context 'mixed content: button & context menu' {
-        It 'runs without errors' {
-            { $Button = New-BTButton -Content 'Google' -Arguments 'https://google.com'
-              $ContextMenuItem = New-BTContextMenuItem -Content 'Bing' -Arguments 'https://bing.com'
-              New-BTAction -Buttons $Button -ContextMenuItems $ContextMenuItem } | Should Not Throw
-        }
-
         $Button = New-BTButton -Content 'Google' -Arguments 'https://google.com'
         $ContextMenuItem = New-BTContextMenuItem -Content 'Bing' -Arguments 'https://bing.com'
 
@@ -93,31 +66,28 @@ Describe 'New-BTAction' {
             New-BTAction -Buttons $Button -ContextMenuItems $ContextMenuItem | Should BeOfType 'Microsoft.Toolkit.Uwp.Notifications.ToastActionsCustom'
         }
 
-        if ($env:TF_BUILD) {
-            Start-Transcript tmp.log
-            try {
-                New-BTAction -Buttons $Button -ContextMenuItems $ContextMenuItem -WhatIf
-            }
-            finally {
-                Stop-Transcript
-                $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
-                Remove-Item tmp.log
-            }
-
-            It 'has consitent WhatIf response' {
-                $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 1 Buttons, and 1 ContextMenuItems".'
-                $Log -join [System.Environment]::NewLine | Should Be $Expected
-            }
+        Start-Transcript tmp.log
+        try {
+            New-BTAction -Buttons $Button -ContextMenuItems $ContextMenuItem -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 1 Buttons, and 1 ContextMenuItems".'
+            $Log -join [System.Environment]::NewLine | Should Be $Expected
         }
     }
 }
 
 Describe 'New-BTAppId' {
-    Context 'running without arguments' {
-        It 'runs without errors' {
-            { New-BTAppId } | Should Not Throw
-        }
+    Mock 'New-Item' {}
+    Mock 'New-ItemProperty' {}
+    Mock 'Test-Path' {return $false}
 
+    Context 'running without arguments' {
         It 'attempts to add item to the registry' {
             Mock 'New-Item' {}
             Mock 'New-ItemProperty' {}
@@ -125,40 +95,25 @@ Describe 'New-BTAppId' {
 
             New-BTAppId
 
-            Assert-MockCalled 'New-Item' -Exactly 1
-            Assert-MockCalled 'New-ItemProperty' -Exactly 1
+            Assert-MockCalled 'New-Item' -Exactly 1 -Scope It
+            Assert-MockCalled 'New-ItemProperty' -Exactly 1 -Scope It
         }
 
-        if ($env:TF_BUILD) {
-            Mock 'New-Item' {}
-            Mock 'New-ItemProperty' {}
-            Mock 'Test-Path' {return $false}
-
-            Start-Transcript tmp.log
-            try {
-                New-BTAppId -WhatIf
-            }
-            finally {
-                Stop-Transcript
-                $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
-                Remove-Item tmp.log
-            }
-
-            It 'has consitent WhatIf response' {
-                $Expected = 'What if: Performing the operation "New-BTAppId" on target "creating: ''HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'' with property ''ShowInActionCenter'' set to ''1'' (DWORD)".'
-                $Log -join [System.Environment]::NewLine | Should Be $Expected
-            }
+        Start-Transcript tmp.log
+        try {
+            New-BTAppId -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BTAppId" on target "creating: ''HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'' with property ''ShowInActionCenter'' set to ''1'' (DWORD)".'
+            $Log -join [System.Environment]::NewLine | Should Be $Expected
         }
     }
     Context 'running with custom AppId' {
-        Mock 'New-Item' {}
-        Mock 'New-ItemProperty' {}
-        Mock 'Test-Path' {return $false}
-
-        It 'runs without errors' {
-            { New-BTAppId -AppId 'Script Checker' } | Should Not Throw
-        }
-
         It 'attempts to add item to the registry' {
             New-BTAppId -AppId 'Script Checker'
 
@@ -166,21 +121,18 @@ Describe 'New-BTAppId' {
             Assert-MockCalled 'New-ItemProperty' -Exactly 1 -Scope It
         }
 
-        if ($env:TF_BUILD) {
-            Start-Transcript tmp.log
-            try {
-                New-BTAppId -AppId 'Script Checker'
-            }
-            finally {
-                Stop-Transcript
-                $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
-                Remove-Item tmp.log
-            }
-
-            It 'has consitent WhatIf response' {
-                $Expected = 'What if: Performing the operation "New-BTAppId" on target "creating: ''HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\Script Checker'' with property ''ShowInActionCenter'' set to ''1'' (DWORD)".'
-                $Log -join [System.Environment]::NewLine | Should Be $Expected
-            }
+        Start-Transcript tmp.log
+        try {
+            New-BTAppId -AppId 'Script Checker'
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BTAppId" on target "creating: ''HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\Script Checker'' with property ''ShowInActionCenter'' set to ''1'' (DWORD)".'
+            $Log -join [System.Environment]::NewLine | Should Be $Expected
         }
     }
 }
