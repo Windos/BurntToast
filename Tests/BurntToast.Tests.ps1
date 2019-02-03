7d9e30dc0f27aa6be8fffa09883341279389542c
Windos <windosbk+github@gmail.com>
2019-02-03 22:01:36 +1300

    Using dashes in asertions

--- Tests/BurntToast.Tests.ps1
+++ Tests/BurntToast.Tests.ps1
@@ -3,7 +3,7 @@ Import-Module "$PSScriptRoot/../BurntToast/BurntToast.psd1" -Force
 Describe 'New-BTAction' {
     Context 'running without arguments' {
         It 'returns a [ToastActionsCustom] object' {
-            New-BTAction | Should BeOfType 'Microsoft.Toolkit.Uwp.Notifications.ToastActionsCustom'
+            New-BTAction | Should -BeOfType 'Microsoft.Toolkit.Uwp.Notifications.ToastActionsCustom'
         }
 
         Start-Transcript tmp.log
@@ -17,12 +17,12 @@ Describe 'New-BTAction' {
         }
         It 'has consitent WhatIf response' {
             $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 0 Buttons, and 0 ContextMenuItems".'
-            $Log -join [System.Environment]::NewLine | Should Be $Expected
+            $Log -join [System.Environment]::NewLine | Should -Be $Expected
         }
     }
     Context 'snooze and dismiss modal' {
         It 'returns a [ToastActionsSnoozeAndDismiss] object' {
-            New-BTAction -SnoozeAndDismiss | Should BeOfType 'Microsoft.Toolkit.Uwp.Notifications.ToastActionsSnoozeAndDismiss'
+            New-BTAction -SnoozeAndDismiss | Should -BeOfType 'Microsoft.Toolkit.Uwp.Notifications.ToastActionsSnoozeAndDismiss'
         }
 
         Start-Transcript tmp.log
@@ -36,12 +36,12 @@ Describe 'New-BTAction' {
         }
         It 'has consitent WhatIf response' {
             $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsSnoozeAndDismiss] with 0 Inputs, 0 Buttons, and 0 ContextMenuItems".'
-            $Log -join [System.Environment]::NewLine | Should Be $Expected
+            $Log -join [System.Environment]::NewLine | Should -Be $Expected
         }
     }
     Context 'single clickable button' {
         It 'returns a [ToastActionsCustom] object' {
-            New-BTAction -Buttons (New-BTButton -Content 'Google' -Arguments 'https://google.com') | Should BeOfType 'Microsoft.Toolkit.Uwp.Notifications.ToastActionsCustom'
+            New-BTAction -Buttons (New-BTButton -Content 'Google' -Arguments 'https://google.com') | Should -BeOfType 'Microsoft.Toolkit.Uwp.Notifications.ToastActionsCustom'
         }
 
         Start-Transcript tmp.log
@@ -55,7 +55,7 @@ Describe 'New-BTAction' {
         }
         It 'has consitent WhatIf response' {
             $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 1 Buttons, and 0 ContextMenuItems".'
-            $Log -join [System.Environment]::NewLine | Should Be $Expected
+            $Log -join [System.Environment]::NewLine | Should -Be $Expected
         }
     }
     Context 'mixed content: button & context menu' {
@@ -63,7 +63,7 @@ Describe 'New-BTAction' {
         $ContextMenuItem = New-BTContextMenuItem -Content 'Bing' -Arguments 'https://bing.com'
 
         It 'returns a [ToastActionsCustom] object' {
-            New-BTAction -Buttons $Button -ContextMenuItems $ContextMenuItem | Should BeOfType 'Microsoft.Toolkit.Uwp.Notifications.ToastActionsCustom'
+            New-BTAction -Buttons $Button -ContextMenuItems $ContextMenuItem | Should -BeOfType 'Microsoft.Toolkit.Uwp.Notifications.ToastActionsCustom'
         }
 
         Start-Transcript tmp.log
@@ -77,7 +77,7 @@ Describe 'New-BTAction' {
         }
         It 'has consitent WhatIf response' {
             $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 1 Buttons, and 1 ContextMenuItems".'
-            $Log -join [System.Environment]::NewLine | Should Be $Expected
+            $Log -join [System.Environment]::NewLine | Should -Be $Expected
         }
     }
 }
@@ -88,11 +88,10 @@ Describe 'New-BTAppId' {
     Mock 'Test-Path' {return $false}
 
     Context 'running without arguments' {
+        It 'checks the mock' {
+            Test-Path 'C:\Demos\01_chatter.ps1' | Should -Be $false
+        }
         It 'attempts to add item to the registry' {
-            Mock 'New-Item' {}
-            Mock 'New-ItemProperty' {}
-            Mock 'Test-Path' {return $false}
-
             New-BTAppId
 
             Assert-MockCalled 'New-Item' -Exactly 1 -Scope It
@@ -110,7 +109,7 @@ Describe 'New-BTAppId' {
         }
         It 'has consitent WhatIf response' {
             $Expected = 'What if: Performing the operation "New-BTAppId" on target "creating: ''HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'' with property ''ShowInActionCenter'' set to ''1'' (DWORD)".'
-            $Log -join [System.Environment]::NewLine | Should Be $Expected
+            $Log -join [System.Environment]::NewLine | Should -Be $Expected
         }
     }
     Context 'running with custom AppId' {
@@ -132,7 +131,7 @@ Describe 'New-BTAppId' {
         }
         It 'has consitent WhatIf response' {
             $Expected = 'What if: Performing the operation "New-BTAppId" on target "creating: ''HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\Script Checker'' with property ''ShowInActionCenter'' set to ''1'' (DWORD)".'
-            $Log -join [System.Environment]::NewLine | Should Be $Expected
+            $Log -join [System.Environment]::NewLine | Should -Be $Expected
         }
     }
 }
@@ -144,20 +143,20 @@ Describe 'New-BTAppId' {
 Describe 'New-BurntToastNotification' {
     Context 'running without arguments' {
         It 'runs without errors' {
-            { New-BurntToastNotification } | Should Not Throw
+            { New-BurntToastNotification } | Should -Not Throw
         }
 
         It 'does not return anything' {
-            New-BurntToastNotification | Should BeNullOrEmpty
+            New-BurntToastNotification | Should -BeNullOrEmpty
         }
     }
     Context 'running with arguments (Image)' {
         It 'runs without errors' {
-            { New-BurntToastNotification -AppLogo 'https://raw.githubusercontent.com/Windos/BurntToast/master/Media/BurntToast-Logo.png' } | Should Not Throw
+            { New-BurntToastNotification -AppLogo 'https://raw.githubusercontent.com/Windos/BurntToast/master/Media/BurntToast-Logo.png' } | Should -Not Throw
         }
 
         It 'does not return anything' {
-            New-BurntToastNotification -AppLogo 'https://raw.githubusercontent.com/Windos/BurntToast/master/Media/BurntToast-Logo.png' | Should BeNullOrEmpty
+            New-BurntToastNotification -AppLogo 'https://raw.githubusercontent.com/Windos/BurntToast/master/Media/BurntToast-Logo.png' | Should -BeNullOrEmpty
         }
     }
 }
