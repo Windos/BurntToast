69f2446918c384a92f5c1b7402a69561ba152130
Windos <windosbk+github@gmail.com>
2019-02-04 13:25:23 +1300

    Tests for New-BTContent

--- Tests/BurntToast.Tests.ps1
+++ Tests/BurntToast.Tests.ps1
@@ -244,6 +244,47 @@ Describe 'New-BTBinding' {
 
 
 
+
+Describe 'New-BTContent' {
+    $Text1 = New-BTText -Content 'This is a test'
+    $Text2 = New-BTText -Content 'This more testing'
+    $Image2 = New-BTImage -Source '$PSScriptRoot\Media\BurntToast.png' -AppLogoOverride -Crop Circle
+    $Binding1 = New-BTBinding -Children $Text1, $Text2 -AppLogoOverride $Image2
+    $Visual1 = New-BTVisual -BindingGeneric $Binding1
+
+    Context 'loaded with children' {
+        Start-Transcript tmp.log
+        try {
+            New-BTContent -Visual $Visual1 -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BTContent" on target "returning: [ToastContent] with XML: <?xml version="1.0" encoding="utf-8"?><toast><visual><binding template="ToastGeneric"><text>{This is a test}</text><text>{This more testing}</text><image src="$PSScriptRoot\Media\BurntToast.png" placement="appLogoOverride" hint-crop="circle" /></binding></visual></toast>".'
+            $Log -join [System.Environment]::NewLine | should Be $Expected
+        }
+    }
+    Context 'built in activation' {
+        Start-Transcript tmp.log
+        try {
+            New-BTContent -Visual $Visual1 -ActivationType Protocol -Launch 'https://google.com' -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BTContent" on target "returning: [ToastContent] with XML: <?xml version="1.0" encoding="utf-8"?><toast activationType="protocol" launch="https://google.com"><visual><binding template="ToastGeneric"><text>{This is a test}</text><text>{This more testing}</text><image src="$PSScriptRoot\Media\BurntToast.png" placement="appLogoOverride" hint-crop="circle" /></binding></visual></toast>".'
+            $Log -join [System.Environment]::NewLine | should Be $Expected
+        }
+    }
+}
+
+
 Describe 'New-BurntToastNotification' {
     Context 'running without arguments' {
         It 'runs without errors' {
