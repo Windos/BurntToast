4581f9813430e004ba54f6f7b19e4b76690e615b
Windos <windosbk+github@gmail.com>
2019-02-04 17:00:47 +1300

    Tests for N-BTN
    Having issues with long output, commiting to see if same on Azure

--- Tests/BurntToast.Tests.ps1
+++ Tests/BurntToast.Tests.ps1
@@ -548,25 +548,100 @@ Describe 'New-BTContent' {
     }
 }
 
-
-
-# Describe 'New-BurntToastNotification' {
-#     Context 'running without arguments' {
-#         It 'runs without errors' {
-#             { New-BurntToastNotification } | should Not Throw
-#         }
-#
-#         It 'does not return anything' {
-#             New-BurntToastNotification | should BeNullOrEmpty
-#         }
-#     }
-#     Context 'running with arguments (Image)' {
-#         It 'runs without errors' {
-#             { New-BurntToastNotification -AppLogo 'https://raw.githubusercontent.com/Windos/BurntToast/master/Media/BurntToast-Logo.png' } | should Not Throw
-#         }
-#
-#         It 'does not return anything' {
-#             New-BurntToastNotification -AppLogo 'https://raw.githubusercontent.com/Windos/BurntToast/master/Media/BurntToast-Logo.png' | should BeNullOrEmpty
-#         }
-#     }
-# }
+Describe 'New-BurntToastNotification' {
+    Context 'running without arguments (default toast)' {
+        Start-Transcript tmp.log
+        try {
+            New-BurntToastNotification -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BurntToastNotification" on target "submitting: [ToastContent] with AppId @{AppId={1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe; AppLogo=\Images\BurntToast.png}.AppId and XML: <?xml version="1.0" encoding="utf-8"?><toast><visual><binding template="ToastGeneric"><text>{Default Notification}</text><image src="C:\Users\joshuak\Documents\GitHub\BurntToast\BurntToast\Images\BurntToast.png" placement="appLogoOverride" hint-crop="circle" /></binding></visual></toast>".'
+            $Log -join [System.Environment]::NewLine | should Be $Expected
+        }
+    }
+    Context 'custom text' {
+        Start-Transcript tmp.log
+        try {
+            New-BurntToastNotification -Text 'Example Script', 'The example script has run successfully.' -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BurntToastNotification" on target "submitting: [ToastContent] with AppId @{AppId={1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe; AppLogo=\Images\BurntToast.png}.AppId and XML: <?xml version="1.0" encoding="utf-8"?><toast><visual><binding template="ToastGeneric"><text>{Example Script}</text><text>{The example script has run successfully.}</text><image src="C:\Users\joshuak\Documents\GitHub\BurntToast\BurntToast\Images\BurntToast.png" placement="appLogoOverride" hint-crop="circle" /></binding></visual></toast>".'
+            $Log -join [System.Environment]::NewLine | should Be $Expected
+        }
+    }
+    Context 'system supplied sound option' {
+        Start-Transcript tmp.log
+        try {
+            New-BurntToastNotification -Text 'WAKE UP!' -Sound 'Alarm2' -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BurntToastNotification" on target "submitting: [ToastContent] with AppId @{AppId={1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe; AppLogo=\Images\BurntToast.png}.AppId and XML: <?xml version="1.0" encoding="utf-8"?><toast duration="long"><visual><binding template="ToastGeneric"><text>{WAKE UP!}</text><image src="C:\Users\joshuak\Documents\GitHub\BurntToast\BurntToast\Images\BurntToast.png" placement="appLogoOverride" hint-crop="circle" /></binding></visual><audio src="ms-winsoundevent:Notification.Looping.Alarm2"
+            loop="true" /></toast>".'
+            $Log -join [System.Environment]::NewLine | should Be $Expected
+        }
+    }
+    Context 'include a button' {
+        Start-Transcript tmp.log
+        try {
+            $BlogButton = New-BTButton -Content 'Open Blog' -Arguments 'https://king.geek.nz'
+            New-BurntToastNotification -Text 'New Blog Post!' -Button $BlogButton -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BurntToastNotification" on target "submitting: [ToastContent] with AppId @{AppId={1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe; AppLogo=\Images\BurntToast.png}.AppId and XML: <?xml version="1.0" encoding="utf-8"?><toast><visual><binding template="ToastGeneric"><text>{New Blog Post!}</text><image src="C:\Users\joshuak\Documents\GitHub\BurntToast\BurntToast\Images\BurntToast.png" placement="appLogoOverride" hint-crop="circle" /></binding></visual><actions><action content="Open Blog" arguments="https://king.geek.nz" activationType="protocol" /></actions></toast>".'
+            $Log -join [System.Environment]::NewLine | should Be $Expected
+        }
+    }
+    Context 'include a header' {
+        Start-Transcript tmp.log
+        try {
+            $ToastHeader = New-BTHeader -Id '001' -Title 'Stack Overflow Questions'
+            New-BurntToastNotification -Text 'New Stack Overflow Question!', 'More details!' -Header $ToastHeader -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BurntToastNotification" on target "submitting: [ToastContent] with AppId @{AppId={1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe; AppLogo=\Images\BurntToast.png}.AppId and XML: <?xml version="1.0" encoding="utf-8"?><toast><visual><binding template="ToastGeneric"><text>{New Stack Overflow Question!}</text><text>{More details!}</text><image src="C:\Users\joshuak\Documents\GitHub\BurntToast\BurntToast\Images\BurntToast.png" placement="appLogoOverride" hint-crop="circle" /></binding></visual><header id="001" title="Stack Overflow Questions" arguments="" activationType="protocol" /></toast>".'
+            $Log -join [System.Environment]::NewLine | should Be $Expected
+        }
+    }
+    Context 'include a progress bar' {
+        Start-Transcript tmp.log
+        try {
+            $Progress = New-BTProgressBar -Status 'Copying files' -Value 0.2
+            New-BurntToastNotification -Text 'File copy script running', 'More details!' -ProgressBar $Progress -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BurntToastNotification" on target "submitting: [ToastContent] with AppId @{AppId={1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe; AppLogo=\Images\BurntToast.png}.AppId and XML: <?xml version="1.0" encoding="utf-8"?><toast><visual><binding template="ToastGeneric"><text>{File copy script running}</text><text>{More details!}</text><progress value="{0.2}" status="{Copying files}" /><image src="C:\Users\joshuak\Documents\GitHub\BurntToast\BurntToast\Images\BurntToast.png" placement="appLogoOverride" hint-crop="circle"
+            /></binding></visual></toast>".'
+            $Log -join [System.Environment]::NewLine | should Be $Expected
+        }
+    }
+}
