0f2f4d7255e26ae9960e99ecf6a53af25e44b850
Windos <windosbk+github@gmail.com>
2019-02-04 13:29:36 +1300

    Tests for New-BTContextMenuItem + re-arrange

--- Tests/BurntToast.Tests.ps1
+++ Tests/BurntToast.Tests.ps1
@@ -1,71 +1,5 @@
 Import-Module "$PSScriptRoot/../BurntToast/BurntToast.psd1" -Force
 
-Describe 'New-BTAction' {
-    Context 'running without arguments' {
-        Start-Transcript tmp.log
-        try {
-            New-BTAction -WhatIf
-        }
-        finally {
-            Stop-Transcript
-            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
-            Remove-Item tmp.log
-        }
-        It 'has consitent WhatIf response' {
-            $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 0 Buttons, and 0 ContextMenuItems".'
-            $Log -join [System.Environment]::NewLine | should Be $Expected
-        }
-    }
-    Context 'snooze and dismiss modal' {
-        Start-Transcript tmp.log
-        try {
-            New-BTAction -SnoozeAndDismiss -WhatIf
-        }
-        finally {
-            Stop-Transcript
-            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
-            Remove-Item tmp.log
-        }
-        It 'has consitent WhatIf response' {
-            $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsSnoozeAndDismiss] with 0 Inputs, 0 Buttons, and 0 ContextMenuItems".'
-            $Log -join [System.Environment]::NewLine | should Be $Expected
-        }
-    }
-    Context 'single clickable button' {
-        Start-Transcript tmp.log
-        try {
-            New-BTAction -Buttons (New-BTButton -Content 'Google' -Arguments 'https://google.com') -WhatIf
-        }
-        finally {
-            Stop-Transcript
-            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
-            Remove-Item tmp.log
-        }
-        It 'has consitent WhatIf response' {
-            $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 1 Buttons, and 0 ContextMenuItems".'
-            $Log -join [System.Environment]::NewLine | should Be $Expected
-        }
-    }
-    Context 'mixed content: button & context menu' {
-        $Button = New-BTButton -Content 'Google' -Arguments 'https://google.com'
-        $ContextMenuItem = New-BTContextMenuItem -Content 'Bing' -Arguments 'https://bing.com'
-
-        Start-Transcript tmp.log
-        try {
-            New-BTAction -Buttons $Button -ContextMenuItems $ContextMenuItem -WhatIf
-        }
-        finally {
-            Stop-Transcript
-            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
-            Remove-Item tmp.log
-        }
-        It 'has consitent WhatIf response' {
-            $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 1 Buttons, and 1 ContextMenuItems".'
-            $Log -join [System.Environment]::NewLine | should Be $Expected
-        }
-    }
-}
-
 Describe 'New-BTAppId' {
     Context 'running without arguments' {
         Start-Transcript tmp.log
@@ -211,10 +145,95 @@ Describe 'New-BTButton' {
     }
 }
 
+Describe 'New-BTContextMenuItem' {
+    Context 'loaded with standard arguments' {
+        Start-Transcript tmp.log
+        try {
+            New-BTContextMenuItem -Content 'Google' -Arguments 'https://google.com' -ActivationType Protocol -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BTContextMenuItem" on target "returning: [ToastContextMenuItem]:Content=Google:Arguments=https://google.com:ActivationType=Protocol".'
+            $Log -join [System.Environment]::NewLine | should Be $Expected
+        }
+    }
+}
 
 
 
 
+
+
+
+Describe 'New-BTAction' {
+    Context 'running without arguments' {
+        Start-Transcript tmp.log
+        try {
+            New-BTAction -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 0 Buttons, and 0 ContextMenuItems".'
+            $Log -join [System.Environment]::NewLine | should Be $Expected
+        }
+    }
+    Context 'snooze and dismiss modal' {
+        Start-Transcript tmp.log
+        try {
+            New-BTAction -SnoozeAndDismiss -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsSnoozeAndDismiss] with 0 Inputs, 0 Buttons, and 0 ContextMenuItems".'
+            $Log -join [System.Environment]::NewLine | should Be $Expected
+        }
+    }
+    Context 'single clickable button' {
+        Start-Transcript tmp.log
+        try {
+            New-BTAction -Buttons (New-BTButton -Content 'Google' -Arguments 'https://google.com') -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 1 Buttons, and 0 ContextMenuItems".'
+            $Log -join [System.Environment]::NewLine | should Be $Expected
+        }
+    }
+    Context 'mixed content: button & context menu' {
+        $Button = New-BTButton -Content 'Google' -Arguments 'https://google.com'
+        $ContextMenuItem = New-BTContextMenuItem -Content 'Bing' -Arguments 'https://bing.com'
+
+        Start-Transcript tmp.log
+        try {
+            New-BTAction -Buttons $Button -ContextMenuItems $ContextMenuItem -WhatIf
+        }
+        finally {
+            Stop-Transcript
+            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+            Remove-Item tmp.log
+        }
+        It 'has consitent WhatIf response' {
+            $Expected = 'What if: Performing the operation "New-BTAction" on target "returning: [ToastActionsCustom] with 0 Inputs, 1 Buttons, and 1 ContextMenuItems".'
+            $Log -join [System.Environment]::NewLine | should Be $Expected
+        }
+    }
+}
 
 Describe 'New-BTBinding' {
     Context 'loaded with children' {
@@ -285,23 +304,23 @@ Describe 'New-BTContent' {
 }
 
 
-Describe 'New-BurntToastNotification' {
-    Context 'running without arguments' {
-        It 'runs without errors' {
-            { New-BurntToastNotification } | should Not Throw
-        }
-
-        It 'does not return anything' {
-            New-BurntToastNotification | should BeNullOrEmpty
-        }
-    }
-    Context 'running with arguments (Image)' {
-        It 'runs without errors' {
-            { New-BurntToastNotification -AppLogo 'https://raw.githubusercontent.com/Windos/BurntToast/master/Media/BurntToast-Logo.png' } | should Not Throw
-        }
-
-        It 'does not return anything' {
-            New-BurntToastNotification -AppLogo 'https://raw.githubusercontent.com/Windos/BurntToast/master/Media/BurntToast-Logo.png' | should BeNullOrEmpty
-        }
-    }
-}
+# Describe 'New-BurntToastNotification' {
+#     Context 'running without arguments' {
+#         It 'runs without errors' {
+#             { New-BurntToastNotification } | should Not Throw
+#         }
+# 
+#         It 'does not return anything' {
+#             New-BurntToastNotification | should BeNullOrEmpty
+#         }
+#     }
+#     Context 'running with arguments (Image)' {
+#         It 'runs without errors' {
+#             { New-BurntToastNotification -AppLogo 'https://raw.githubusercontent.com/Windos/BurntToast/master/Media/BurntToast-Logo.png' } | should Not Throw
+#         }
+#
+#         It 'does not return anything' {
+#             New-BurntToastNotification -AppLogo 'https://raw.githubusercontent.com/Windos/BurntToast/master/Media/BurntToast-Logo.png' | should BeNullOrEmpty
+#         }
+#     }
+# }
