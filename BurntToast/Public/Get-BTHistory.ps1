64227188ee2114cd16f8d64bcb5b43d715cf1957
Windos <windosbk+github@gmail.com>
2019-03-26 20:11:08 +1300

    Get history, fintered by unique id

--- BurntToast/Public/Get-BTHistory.ps1
+++ BurntToast/Public/Get-BTHistory.ps1
@@ -21,12 +21,20 @@ function Get-BTHistory {
 
     param (
         # Specifies the AppId of the 'application' or process that spawned the toast notification.
-        [string] $AppId = $Script:Config.AppId
+        [string] $AppId = $Script:Config.AppId,
+
+        [string] $UniqueIdentifier
     )
 
     if (!(Test-Path -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\$AppId")) {
         throw "The AppId $AppId is not present in the registry, please run New-BTAppId to avoid inconsistent Toast behaviour."
     } else {
-        [Windows.UI.Notifications.ToastNotificationManager]::History.GetHistory($AppId)
+        $History = [Windows.UI.Notifications.ToastNotificationManager]::History.GetHistory($AppId)
+
+        if ($UniqueIdentifier) {
+            $History | Where-Object {$_.Tag -eq $UniqueIdentifier -or $_.Group -eq $UniqueIdentifier}
+        } else {
+            $History
+        }
     }
 }
