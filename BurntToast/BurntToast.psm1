360ba6531013157a5fcaf770203ca127485aadcf
Windos <windosbk+github@gmail.com>
2020-03-01 22:43:47 +1300

    OTSB!

--- BurntToast/BurntToast.psm1
+++ BurntToast/BurntToast.psm1
@@ -1,8 +1,7 @@
 $WinMajorVersion = (Get-CimInstance -ClassName Win32_OperatingSystem -Property Version).Version.Split('.')[0]
 
-if ($WinMajorVersion -ge 10)
-{
-    $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+if ($WinMajorVersion -ge 10) {
+    $Public = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
     $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
     $Library = @( Get-ChildItem -Path $PSScriptRoot\lib\*.dll -Recurse -ErrorAction SilentlyContinue )
 
@@ -13,26 +12,18 @@ if ($WinMajorVersion -ge 10)
         $Script:Config.AppLogo
     }
 
-    Foreach($Import in @($Public + $Private))
-    {
-        Try
-        {
+    foreach ($Import in @($Public + $Private)) {
+        try {
             . $Import.FullName
-        }
-        Catch
-        {
+        } catch {
             Write-Error -Message "Failed to import function $($Import.FullName): $_"
         }
     }
 
-    Foreach($Type in $Library)
-    {
-        Try
-        {
+    foreach ($Type in $Library) {
+        try {
             Add-Type -Path $Type.FullName
-        }
-        Catch
-        {
+        } catch {
             Write-Error -Message "Failed to load library $($Type.FullName): $_"
         }
     }
@@ -44,8 +35,6 @@ if ($WinMajorVersion -ge 10)
     New-BTAppId
 
     $null = [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]
-}
-else
-{
+} else {
     throw 'This version of BurntToast will only work on Windows 10. If you would like to use BurntToast on Windows 8, please use version 0.4'
 }
