a44cb5cc88860cf50f3442db3efc133596b1e6b6
Windos <windosbk+github@gmail.com>
2020-04-09 21:20:27 +1200

    Add switch to return snozed toasts

--- BurntToast/Public/Get-BTHistory.ps1
+++ BurntToast/Public/Get-BTHistory.ps1
@@ -8,11 +8,14 @@ function Get-BTHistory {
 
         The object returned includes tag and group information which can be used with the Remove-BTNotification function to clear specific notification from the Action Center.
 
+        Using the ScheduledToast switch you can get all toast notifications that have been scheduled to display, whether by scheduling outright or snoozing.
+
         .INPUTS
         STRING
 
         .OUTPUTS
-        TODO
+        ToastNotification
+        ScheduledToastNotification
 
         .EXAMPLE
         Get-BTHistory
@@ -28,18 +31,25 @@ function Get-BTHistory {
         # A string that uniquely identifies a toast notification. Submitting a new toast with the same identifier as a previous toast will replace the previous toast.
         #
         # This is useful when updating the progress of a process, using a progress bar, or otherwise correcting/updating the information on a toast.
-        [string] $UniqueIdentifier
+        [string] $UniqueIdentifier,
+
+        # Specified that you need to see scheduled toast notifications, rather the those in the Action Center.
+        [switch] $ScheduledToast
     )
 
     if (!(Test-Path -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\$AppId")) {
         throw "The AppId $AppId is not present in the registry, please run New-BTAppId to avoid inconsistent Toast behaviour."
     } else {
-        $History = [Windows.UI.Notifications.ToastNotificationManager]::History.GetHistory($AppId)
+        $Toasts = if ($ScheduledToast) {
+            [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($AppId).GetScheduledToastNotifications()
+        } else {
+            [Windows.UI.Notifications.ToastNotificationManager]::History.GetHistory($AppId)
+        }
 
         if ($UniqueIdentifier) {
-            $History | Where-Object {$_.Tag -eq $UniqueIdentifier -or $_.Group -eq $UniqueIdentifier}
+            $Toasts | Where-Object {$_.Tag -eq $UniqueIdentifier -or $_.Group -eq $UniqueIdentifier}
         } else {
-            $History
+            $Toasts
         }
     }
 }
