426ee8c2ee56ebc3f5f214e770a2033264f3aa9d
Josh (Windos) King <windosbk+github@gmail.com>
2020-09-24 20:07:56 +1200

    Tests and documentation for columns

--- BurntToast/Public/New-BTColumn.ps1
+++ BurntToast/Public/New-BTColumn.ps1
@@ -1,57 +1,92 @@
 function New-BTColumn {
     <#
         .SYNOPSIS
-        Creates a new Text Element for Toast Notifications.
+        Creates a new Column Element for Toast Notifications.
 
         .DESCRIPTION
-        The New-BTText function creates a new Text Element for Toast Notifications.
+        The New-BTColumn function creates a new Column Element, or Adaptive Subgroup, for Toast Notifications.
 
-        You can specify the text you want displayed in a Toast Notification as a string, or run the function without a paramter for a blank line.
+        Valid content for an Adaptive Subgroup include Adaptive Text (New-BTText) and Adaptive Images (New-BTImage).
 
-        Each Text Element is the equivalent of one line in on a Toast Notification, long lines will wrap.
+        These columns are supplied to the Column parameter on either the New-BTBinding or New-BurntToastNotification functions.
 
         .INPUTS
-        String
+        int
+        Microsoft.Toolkit.Uwp.Notifications.IAdaptiveSubgroupChild
+        Microsoft.Toolkit.Uwp.Notifications.AdaptiveSubgroupTextStacking
 
         You cannot pipe input to this function.
 
         .OUTPUTS
-        Text
+        Microsoft.Toolkit.Uwp.Notifications.AdaptiveSubgroup
 
         .EXAMPLE
-        New-BTText -Content 'This is a line with text!'
+        PS C:\>$HeadingText = New-BTText -Text 'Now Playing'
 
-        Creates a Text Element that will show the string 'This is a line with text!' on a Toast Notification.
+        PS C:\>$TitleLabel = New-BTText -Text 'Title:' -Style Base
+        PS C:\>$AlbumLabel = New-BTText -Text 'Album:' -Style Base
+        PS C:\>$ArtistLabel = New-BTText -Text 'Artist:' -Style Base
+
+        PS C:\>$Title = New-BTText -Text 'soft focus' -Style BaseSubtle
+        PS C:\>$Album = New-BTText -Text 'Birocratic' -Style BaseSubtle
+        PS C:\>$Artist = New-BTText -Text 'beets 4 (2017)' -Style BaseSubtle
+
+        PS C:\>$Column1 = New-BTColumn -Children $TitleLabel, $AlbumLabel, $ArtistLabel -Weight 4
+        PS C:\>$Column2 = New-BTColumn -Children $Title, $Album, $Artist -Weight 6
+
+        PS C:\>$Binding1 = New-BTBinding -Children $HeadingText -Column $Column1, $Column2
+        PS C:\>$Visual1 = New-BTVisual -BindingGeneric $Binding1
+        PS C:\>$Content1 = New-BTContent -Visual $Visual1
+
+        PS C:\>Submit-BTNotification -Content $Content1
+
+        This example results in a toast notification displaying hard coded music information.
+
+        Two columns are created, the first containing three labels with the 'Base' text style,
+        the second column contains the corresponding three peices of data that match the labels.
+
+        These columns are have relative weights, making the label column smaller than the data column.
+
+        The columns are added to the toast notification via the Column parameter of the New-BTBinding
+        function.
 
         .EXAMPLE
-        New-BTText
+        PS C:\>$TitleLabel = New-BTText -Text 'Title:' -Style Base
+        PS C:\>$AlbumLabel = New-BTText -Text 'Album:' -Style Base
+        PS C:\>$ArtistLabel = New-BTText -Text 'Artist:' -Style Base
 
-        Creates a Text Element that will show a blank line on a Toast Notification.
+        PS C:\>$Title = New-BTText -Text 'soft focus' -Style BaseSubtle
+        PS C:\>$Album = New-BTText -Text 'Birocratic' -Style BaseSubtle
+        PS C:\>$Artist = New-BTText -Text 'beets 4 (2017)' -Style BaseSubtle
+
+        PS C:\>$Column1 = New-BTColumn -Children $TitleLabel, $AlbumLabel, $ArtistLabel -Weight 4
+        PS C:\>$Column2 = New-BTColumn -Children $Title, $Album, $Artist -Weight 6
+
+        PS C:\>New-BurntToastNotification -Text 'Now Playing' -Column $Column1, $Column2
+
+        This example is similar to the first, except that the toast notification is created via the
+        New-BurntToastNotification function rather than the component functions.
 
         .NOTES
-        TODO: Implement hint-style (https://blogs.msdn.microsoft.com/tiles_and_toasts/2015/06/30/adaptive-tile-templates-schema-and-documentation/)
+        Credit for most of the help text for this function go to the authors of the WindowsCommunityToolkit library that this module relies upon.
 
-        Credit for most of the help text for this function go to the authors of the UWPCommunityToolkit library that this module relies upon.
-
-        Please see the originating repo here: https://github.com/Microsoft/UWPCommunityToolkit
+        Please see the originating repo here: https://github.com/windows-toolkit/WindowsCommunityToolkit
 
         .LINK
-        https://github.com/Windos/BurntToast/blob/main/Help/New-BTText.md
+        https://github.com/Windos/BurntToast/blob/main/Help/New-BTColumn.md
     #>
 
     # [CmdletBinding(SupportsShouldProcess = $true)]
     [OutputType([Microsoft.Toolkit.Uwp.Notifications.AdaptiveSubgroup])]
     param (
-        # The text to display. Data binding support added in Creators Update, only works for toast top-level text elements (But appears to not be working via PowerShell yet.)
+        # The content to be contained within the column. Can contain text (New-BTText) and images (New-BTImage).
         [Parameter()]
         [Microsoft.Toolkit.Uwp.Notifications.IAdaptiveSubgroupChild[]] $Children,
 
-        # The maximum number of lines the text element is allowed to display. For Toasts, top-level text elements will have varying max line amounts (and in the Anniversary Update you can change the max lines).
-        #
-        # Text on a Toast inside a group (not yet implemented) will behave identically to Tiles (default to infinity).
+        # Controls the width of this column in relation to other columns. A higher relative weight results in the wider column.
         [int] $Weight,
 
-        # The minimum number of lines the text element must display. Note that for Toast, this property will only take effect if the text is inside a group (not yet implemented.)
+        # The vertical alignment of the content inside this column.
         [Microsoft.Toolkit.Uwp.Notifications.AdaptiveSubgroupTextStacking] $TextStacking
     )
 
