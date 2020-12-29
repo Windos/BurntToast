c3a0719aca9d720bcee410317bd86b740133f368
Josh King <windosbk+github@gmail.com>
2020-12-29 14:37:02 +1300

    Opps

--- Tests/Get-BTHistory.Tests.ps1
+++ Tests/Get-BTHistory.Tests.ps1
@@ -1,17 +1,19 @@
 . (Join-Path -Path $PSScriptRoot -ChildPath '_envPrep.ps1')
 
 Describe 'Get-BTHistory' {
-    Context 'valid AppId, one previous toast' {
-        Mock Test-Path { $true } -ModuleName BurntToast -Verifiable -ParameterFilter {
-            $Path -eq 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
-        }
+    if ($PlatformAvailable) {
+        Context 'valid AppId, one previous toast' {
+            Mock Test-Path { $true } -ModuleName BurntToast -Verifiable -ParameterFilter {
+                $Path -eq 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Notifications\Settings\{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
+            }
 
-        It 'should not throw' {
-            { Get-BTHistory } | Should -Not -Throw
-        }
+            It 'should not throw' {
+                { Get-BTHistory } | Should -Not -Throw
+            }
 
-        It 'tested the correct path' {
-            Assert-VerifiableMock
+            It 'tested the correct path' {
+                Assert-VerifiableMock
+            }
         }
     }
 
