814d371af0e062463cff442baadd51d7b4f3bd22
Josh King <windosbk+github@gmail.com>
2024-01-13 12:56:51 +1300

    (build) Update tests

--- Tests/New-BTContextMenuItem.Tests.ps1
+++ Tests/New-BTContextMenuItem.Tests.ps1
@@ -1,16 +1,29 @@
-. (Join-Path -Path $PSScriptRoot -ChildPath '_envPrep.ps1')
+BeforeAll {
+    if (Get-Module -Name 'BurntToast') {
+        Remove-Module -Name 'BurntToast'
+    }
+
+    if ($ENV:BURNTTOAST_MODULE_ROOT) {
+        Import-Module $ENV:BURNTTOAST_MODULE_ROOT -Force
+    } else {
+        Import-Module "$PSScriptRoot/../src/BurntToast.psd1" -Force
+    }
+}
 
 Describe 'New-BTContextMenuItem' {
     Context 'loaded with standard arguments' {
-        Start-Transcript tmp.log
-        try {
-            New-BTContextMenuItem -Content 'Google' -Arguments 'https://google.com' -ActivationType Protocol -WhatIf
+        BeforeAll {
+            Start-Transcript tmp.log
+            try {
+                New-BTContextMenuItem -Content 'Google' -Arguments 'https://google.com' -ActivationType Protocol -WhatIf
+            }
+            finally {
+                Stop-Transcript
+                $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
+                Remove-Item tmp.log
+            }
         }
-        finally {
-            Stop-Transcript
-            $Log = (Get-Content tmp.log).Where({ $_ -match "What if: " })
-            Remove-Item tmp.log
-        }
+
         It 'has consitent WhatIf response' {
             $Expected = 'What if: Performing the operation "New-BTContextMenuItem" on target "returning: [ToastContextMenuItem]:Content=Google:Arguments=https://google.com:ActivationType=Protocol".'
             $Log | Should -Be $Expected
